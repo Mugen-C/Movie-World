4c26ac9efcafa93d62d45e8a57bcf25a9d2fec41
Mugen-C <cherifvamussa@gmail.com>
2024-10-01 18:27:35 +0100

    Update api.js

--- MovieWorld/assets/js/api.js
+++ MovieWorld/assets/js/api.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const api_key = 'c98f76e33b5ba3aa1df9122d189175ee';
+const api_key = 'key';
 const imageBaseURL = 'https://image.tmdb.org/t/p/';
 const fetchDataFromServer = function (url, callback, optionalParam) {
   fetch(url)
